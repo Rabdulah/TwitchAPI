11a2715044020d44231c679223b1f84ef0556c4b
CanadianThunder <ramzi_abdullahi@outlook.com>
2018-07-17 16:56:35 -0400

    Changed default route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Homepage from '@/components/Homepage'
 import StreamerCard from '@/components/StreamerCard'
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Homepage',
-      component: Homepage
-    },
-    {
-      path: '/streamerCard',
       name: 'StreamerCard',
       component: StreamerCard
     }
